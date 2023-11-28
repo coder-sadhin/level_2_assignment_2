411f2c276643e8ba6e370a9deefe99de523a7176
coder-sadhin <codersadhin@gmail.com>
2023-11-28 12:27:16 +0600

    backend start now, its live

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application route
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    Operation: 'Success',
+  });
+});
+
+export default app;
